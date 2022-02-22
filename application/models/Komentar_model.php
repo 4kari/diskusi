c22213cc1bd86ee5b91799e430bc93a6c224ac04
4kari <mypasshidden@gmail.com>
2022-02-22 13:50:49 +0700

    update ip

--- application/models/Komentar_model.php
+++ application/models/Komentar_model.php
@@ -1,14 +1,14 @@
 <?php
 class Komentar_model extends CI_Model{
-    protected $ipSkripsi='http://10.5.12.24/skripsi/api/';
-    protected $ipPenjadwalan='http://10.5.12.82/penjadwalan/api/';
-    protected $ipDiskusi='http://10.5.12.56/diskusi/api/';
-    protected $ipUser='http://10.5.12.18/user/api/';
+    // protected $ipSkripsi='http://10.5.12.24/skripsi/api/';
+    // protected $ipPenjadwalan='http://10.5.12.82/penjadwalan/api/';
+    // protected $ipDiskusi='http://10.5.12.56/diskusi/api/';
+    // protected $ipUser='http://10.5.12.18/user/api/';
 
-    // protected $ipSkripsi='http://localhost/microservice/skripsi/api/';
-    // protected $ipPenjadwalan='http://localhost/microservice/penjadwalan/api/';
-    // protected $ipDiskusi='http://localhost/microservice/diskusi/api/';
-    // protected $ipUser='http://localhost/microservice/user/api/';
+    protected $ipSkripsi='http://localhost:8080/microservice/skripsi/api/';
+    protected $ipPenjadwalan='http://localhost:8080/microservice/penjadwalan/api/';
+    protected $ipDiskusi='http://localhost:8080/microservice/diskusi/api/';
+    protected $ipUser='http://localhost:8080/microservice/user/api/';
     
     public function getKomentar(){
         return $this->db->get('komentar')->result_array();
